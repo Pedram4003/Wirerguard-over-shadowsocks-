fb7ea59e5a56ccf886f227eb2cb004fe8904ab6e
Pedram4003 <156018402+Pedram4003@users.noreply.github.com>
2024-09-12 15:30:46 +0330

    Update install-wireguard-shadowsocks.sh

--- install-wireguard-shadowsocks.sh
+++ install-wireguard-shadowsocks.sh
@@ -5,12 +5,7 @@ echo "Shadowsocks and WireGuard installed. Configuring..."
 # Shadowsocks config
 cat <<EOF > /etc/shadowsocks-libev/config.json
 {
-  "server":"185.245.83.26",
-  "server_port":2097,
-  "password":"pedrammmmmm-4003",
-  "method":"aes-256-gcm"
-}
-EOF
+  "server
 systemctl start shadowsocks-libev
 systemctl enable shadowsocks-libev
 # WireGuard config
